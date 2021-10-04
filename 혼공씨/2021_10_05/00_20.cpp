925e32b0886e6a87d647ee60afbc61bf5a86613c
leeur7120 <84216892+leeur7120@users.noreply.github.com>
2021-10-05 01:30:29 +0900

    Add files via upload

---
+++ 혼공씨/2021_10_05/00_20.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h> // 추가한 코드
+
+int main(void)
+{
+    printf("Be happy!");
+    system("pause"); // 추가한 코드
+
+    return 0;
+}
+
+// 실행결과
+
+// Be happy!계속하려면 아무 키나 누르십시오...
